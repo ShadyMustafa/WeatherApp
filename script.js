e6c8eeb1a741b99980b9b2c99d274a7b69c56478
Shady <shadymustafa79@gmail.com>
2024-09-24 03:59:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const apiKey = '08b98807dafbbc45baeaa3e16fedfade';
+
+function getWeather(){
+    const city = document.getElementById('cityInput').value
+    if(!city) return alert('Please type Any City');
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+    fetch(apiUrl)
+        .then(response=>response.json())
+        .then((data)=>displayWeather(data))
+        .catch(error=>alert('city Not found'))
+}
+
+function displayWeather(data){
+    document.getElementById('cityName').textContent = data.name
+    document.getElementById('weatherCondition').textContent=`Condition: ${data.weather[0].description}`
+    document.getElementById('temp').textContent= `Temperature: ${data.main.temp}°C`
+    document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`
+    const timestamp = data.dt;
+    const timezoneOffset = data.timezone;
+
+    
+    const localTime = new Date((timestamp + timezoneOffset) * 1000);
+
+
+    const hours = localTime.getUTCHours().toString().padStart(2, '0'); 
+    const minutes = localTime.getUTCMinutes().toString().padStart(2, '0');
+    const day = localTime.toLocaleString('default', { weekday: 'long' });
+    const fullDate = localTime.toLocaleDateString();
+
+
+    document.getElementById('time').textContent = `Local Time: ${hours}:${minutes}`;
+    document.getElementById('date').textContent = `Day: ${day}, Date: ${fullDate}`;
+}
+
+
